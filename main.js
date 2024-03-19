eaf5f63e7b681a1a5b4d18637360e62503b8d5bf
prfD <dung.nhv.d@gmail.com>
2024-03-19 20:36:36 +0700

    Create  project

---
+++ main.js
@@ -0,0 +1,21 @@
+// alert("Hi Javascript alert!")
+var fullName = "Nguyen Huy Vu Dung";
+var age =  26;
+// alert(fullName);
+// alert(age);
+
+// console.log('Day la 1 dong log');
+// console.error(fullName);
+// confirm('Xac nhan ban du tuoi');
+// prompt('Hello.Say something');
+// setTimeout(function(){
+//     alert('Thong bao')
+// },1000);
+// setInterval( function(){
+//     console.log('Day la log' + Math.random)
+// },1500);
+// tuoi  = parseInt(prompt("Enter your age")) ;
+// if (tuoi > 18)  {
+//     document.write("Your Age is: " + tuoi);
+
+}
